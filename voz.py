b80c03764b6bdee3f74401e566abe31e9834c495
IoavM <132418727+IoavM@users.noreply.github.com>
2024-08-15 07:16:52 -0500

    Update voz.py

--- voz.py
+++ voz.py
@@ -4,4 +4,6 @@ from PIL import Image
 st.title('Primer APP')
 st.header('Primera web de interfaces multimodales')
 st.write('a')
+image = Image.open('perro.jpg')
+st.image(image,caption='perro.jpg')
 
